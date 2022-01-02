a5217584306d80e67f1a2edfb458b5a25675c97f
J'mari Wyatt <60311148+fffilps@users.noreply.github.com>
2022-01-02 15:02:59 -0600

    Create Counting-Sheep.js
    My Solution to Codewars Counting-Sheep Challenge in Javascript

---
+++ Counting-Sheep.js
@@ -0,0 +1,8 @@
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  let count = 0
+  for(let i = 0; i < arrayOfSheep.length; i++){
+    arrayOfSheep[i] == true ? count++ : null
+  }
+    return count
+}
