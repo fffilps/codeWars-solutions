a8c67a0ac220aa054812d064195e79699b207878
J'mari Wyatt <60311148+fffilps@users.noreply.github.com>
2022-01-02 15:14:50 -0600

    Create Format a string of names like 'Bart, Lisa & Maggie'.js
    My solution to Codewars Format a string of names like 'Bart, Lisa & Maggie' challenge in Javascript

---
+++ Format a string of names like 'Bart, Lisa & Maggie'.js
@@ -0,0 +1,15 @@
+function list(names){
+  let str = '';
+  if (names.length !== 0) {
+    let last = names.pop();
+    str = names.map( (val, i, arr) => {
+      if (i !== arr[arr.length - 1]) {
+        return val.name;
+      }
+    }).join(', ')
+     
+    str += str !== '' ? ' & ' + last.name : last.name;
+  }
+   
+  return str;
+}
